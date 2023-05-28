a945b4608e918cdd0529bfcce0c41dd050e06c9b
Carlos Jorge <carlos@graphops.xyz>
2023-05-28 12:26:27 +0100

    .

---
+++ .github/actions/query-releases/src/index.js
@@ -0,0 +1,53 @@
+const { GitHub, getOctokitOptions } = require("@actions/github/lib/utils");
+const { paginateGraphql } = require("@octokit/plugin-paginate-graphql");
+const octokit = GitHub.plugin(paginateGraphql)
+const core = require('@actions/core');
+
+const token = process.env.GITHUB_TOKEN;
+const myOctokit = new octokit(getOctokitOptions(token))
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+async function query() {
+  const response = await myOctokit.graphql.paginate(`
+  query paginate($cursor: String, $owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+    releases(
+        first: 2
+        after: $cursor
+        orderBy: {field: CREATED_AT, direction: ASC}
+    ) {
+        edges {
+        node {
+            name
+            isPrerelease
+            description
+            createdAt
+            releaseAssets(last: 100) {
+            nodes {
+                createdAt
+                name
+                size
+                downloadUrl
+            }
+            }
+        }
+        }
+        pageInfo {
+        endCursor
+        hasNextPage
+        }
+    }
+  }
+}`,
+{
+  owner: owner,
+  name: repo
+}
+);
+
+  console.log("${response}");
+  core.setOutput('response', response);
+
+}
+
+query();
