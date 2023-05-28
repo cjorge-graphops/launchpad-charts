24420651ced8ea3737cc4f9ae067bff05e2691f3
Carlos Jorge <carlos@graphops.xyz>
2023-05-28 13:37:14 +0100

    .

--- .github/actions/query-releases/src/index.js
+++ .github/actions/query-releases/src/index.js
@@ -1,20 +1,23 @@
 const { GitHub, getOctokitOptions } = require("@actions/github/lib/utils");
 const { paginateGraphql } = require("@octokit/plugin-paginate-graphql");
-const octokit = GitHub.plugin(paginateGraphql)
+const octokit = GitHub.plugin(paginateGraphql);
 const core = require('@actions/core');
+const artifact = require('@actions/artifact');
 
 const token = core.getInput('token');
 const owner = core.getInput('owner');
 const repo = core.getInput('repo');
 
-const myOctokit = new octokit(getOctokitOptions(token))
+const pagOctokit = new octokit(getOctokitOptions(token))
+
+const fs = require('fs');
 
 async function query() {
-  const response = await myOctokit.graphql.paginate(`
+  const response = await pagOctokit.graphql.paginate(`
   query paginate($cursor: String, $owner: String!, $name: String!) {
     repository(owner: $owner, name: $name) {
     releases(
-        first: 2
+        first: 100
         after: $cursor
         orderBy: {field: CREATED_AT, direction: ASC}
     ) {
@@ -24,7 +27,7 @@ async function query() {
             isPrerelease
             description
             createdAt
-            releaseAssets(last: 100) {
+            releaseAssets(last: 1) {
             nodes {
                 createdAt
                 name
@@ -48,8 +51,22 @@ async function query() {
 );
 
   console.log("${response}");
+
   core.setOutput('response', response);
 
+  fs.writeFileSync('./releases.json', response)
+
+  const artifactClient = artifact.create()
+  const options = {
+    continueOnError: false
+  }
+
+  const uploadResponse = await artifactClient.uploadArtifact(
+    'releases',
+    'releases.json',
+    '.',
+    options
+  )
 }
 
 query();
